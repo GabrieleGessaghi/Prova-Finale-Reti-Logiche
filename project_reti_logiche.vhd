da5ac741b3b057ced54a179a621a88ed7c429d2f
Gabriele Gessaghi <gabrielegessaghi@gmail.com>
2023-03-01 16:39:41 +0100

    edit

--- project_reti_logiche.vhd
+++ project_reti_logiche.vhd
@@ -31,31 +31,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity project_reti_logiche is
-    port (
-             i_clk   : in std_logic;
-             i_rst   : in std_logic;
-             i_start : in std_logic;
-             i_w     : in std_logic;
-             o_z0    : out std_logic_vector(7 downto 0);
-             o_z1    : out std_logic_vector(7 downto 0);
-             o_z2    : out std_logic_vector(7 downto 0);
-             o_z3    : out std_logic_vector(7 downto 0);
-             o_done  : out std_logic;
-             o_mem_addr : out std_logic_vector(15 downto 0);
-             i_mem_data : in std_logic_vector(7 downto 0);
-             o_mem_we   : out std_logic;
-             o_mem_en   : out std_logic
-         );
-end project_reti_logiche;
-
-architecture Behavioral of project_reti_logiche is
-
-begin
-
-
-end Behavioral;
-
 
 entity datapath is 
     port(
@@ -81,3 +56,29 @@ architecture Behavioral of fsm is
 begin
 
 end Behavioral;
+
+entity project_reti_logiche is
+    port (
+             i_clk   : in std_logic;
+             i_rst   : in std_logic;
+             i_start : in std_logic;
+             i_w     : in std_logic;
+             o_z0    : out std_logic_vector(7 downto 0);
+             o_z1    : out std_logic_vector(7 downto 0);
+             o_z2    : out std_logic_vector(7 downto 0);
+             o_z3    : out std_logic_vector(7 downto 0);
+             o_done  : out std_logic;
+             o_mem_addr : out std_logic_vector(15 downto 0);
+             i_mem_data : in std_logic_vector(7 downto 0);
+             o_mem_we   : out std_logic;
+             o_mem_en   : out std_logic
+         );
+end project_reti_logiche;
+
+architecture Behavioral of project_reti_logiche is
+
+begin
+
+
+end Behavioral;
+
