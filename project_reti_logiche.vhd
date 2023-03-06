05ba8e0334e27a25c7ceab426f95b72c1b2f9a12
Gabriele Gessaghi <gabrielegessaghi@gmail.com>
2023-03-06 12:09:29 +0100

    reduced mux number

--- project_reti_logiche.vhd
+++ project_reti_logiche.vhd
@@ -25,38 +25,6 @@ USE IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 -- use IEEE.STD_LOGIC_UNSIGNED.all;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
--- DATAPATH in
--- i_clk
--- i_res
--- address_enable
--- channel_enable
--- done
--- receive
--- i_w
--- i_mem_data
-
--- DATAPATH out
--- o_mem_address
--- o_z1
--- o_z2
--- o_z3
--- o_z4
-
--- DATAPATH internal
--- sum_address
--- mult_address
--- sum_channel
--- channel_selector
--- mult_channel
--- reg_enable
--- o_reg_z1
--- o_reg_z2
--- o_reg_z3
--- o_reg_z4
 
 ENTITY project_reti_logiche IS
     PORT (
@@ -84,8 +52,14 @@ ARCHITECTURE Behavioral OF project_reti_logiche IS
     SIGNAL addr_en : STD_LOGIC;
     SIGNAL chan_en : STD_LOGIC;
     SIGNAL receive : STD_LOGIC;
+    -- ch_enable pack
+    SIGNAL ch0_en : STD_LOGIC;
+    SIGNAL ch1_en : STD_LOGIC;
+    SIGNAL ch2_en : STD_LOGIC;
+    SIGNAL ch3_en : STD_LOGIC;
     SIGNAL internal_rst : STD_LOGIC;
     SIGNAL temp_done : STD_LOGIC;
+    signal ingresso : std_logic;
     
     SIGNAL reg_z0 : STD_LOGIC_VECTOR (7 DOWNTO 0);
     SIGNAL reg_z1 : STD_LOGIC_VECTOR (7 DOWNTO 0);
@@ -98,23 +72,32 @@ ARCHITECTURE Behavioral OF project_reti_logiche IS
     
 BEGIN
     
-    -- calcolo indirizzo, calcolo canale, output canale, output indirizzo
-    PROCESS (i_clk, i_rst)
+    PROCESS(i_clk)
+    begin
+        if rising_edge(i_clk) then ingresso <= i_w; end if;
+    end process;
+    
+    PROCESS (i_clk, i_rst, internal_rst) -- processo calcolo address
+    BEGIN
+        IF (i_rst = '1') or internal_rst = '1' THEN
+            sum_address <= (others => '0');
+        ELSIF rising_edge(i_clk) and addr_en = '1' THEN
+            -- sum_address <= std_logic_vector(unsigned(sum_address) sll 1); -- shift logico sx del canale
+            -- sum_address(0) <= '1';
+            sum_address <= sum_address(14 downto 0) & ingresso;
+        END IF;
+    END PROCESS;
+    
+    o_mem_addr <= sum_address;
+
+    PROCESS (i_clk, i_rst, internal_rst) -- processo selezione canale di uscita
     BEGIN
         IF (i_rst = '1') or internal_rst = '1' THEN
             channel_selector <= (others => '0');
-            o_mem_addr <= (others => '0');
         ELSIF rising_edge(i_clk) and chan_en = '1' THEN
-            sum_channel <= ("0" & i_w); 
-            if state = S1 then
-                sum_channel <= std_logic_vector(unsigned(sum_channel) sll 1); -- shift logico sx del canale
-            else
-                channel_selector <= sum_channel;
-            end if;
-        ELSIF rising_edge(i_clk) and addr_en = '1' THEN
-            sum_address <= std_logic_vector(unsigned(sum_address) sll 1); -- shift logico sx del canale
-            sum_address(0) <= i_w;
-            o_mem_addr <= sum_address;
+            -- channel_selector <= std_logic_vector(unsigned(channel_selector) sll 1); -- shift logico sx del canale
+            -- channel_selector(0) <= '1';
+            channel_selector <= channel_selector(0) & ingresso;
         END IF;
     END PROCESS;
 
@@ -126,20 +109,43 @@ BEGIN
             reg_z1 <= "00000000";
             reg_z2 <= "00000000";
             reg_z3 <= "00000000";
-        elsif receive = '1' and channel_selector = "00" then 
-            reg_z0 <= i_mem_data;
-        elsif receive = '1' and channel_selector = "01" then 
-            reg_z1 <= i_mem_data;
-        elsif receive = '1' and channel_selector = "10" then 
-            reg_z2 <= i_mem_data;
-        elsif receive = '1' and channel_selector = "11" then 
-            reg_z3 <= i_mem_data;
+        else 
+            --if receive = '1' then
+                if ch0_en = '1' then 
+                    reg_z0 <= i_mem_data;
+                end if; 
+                if ch1_en = '1' then 
+                    reg_z1 <= i_mem_data;
+                end if;
+                if ch2_en = '1' then 
+                    reg_z2 <= i_mem_data;
+                end if;
+                if ch3_en = '1' then 
+                    reg_z3 <= i_mem_data;
+                end if;
+            --end if;
         end if;
     end process;
     
-
+    process(i_clk, receive) -- selezione canale uscita ed enabler mux
+    begin
+        ch0_en <= '0'; 
+        ch1_en <= '0'; 
+        ch2_en <= '0'; 
+        ch3_en <= '0'; 
+        if receive = '1' then
+            CASE channel_selector IS 
+                when "00" => ch0_en <= '1' ;
+                when "01" => ch1_en <= '1' ;
+                when "10" => ch2_en <= '1' ;
+                when "11" => ch3_en <= '1' ;
+                when others => null ;
+            END CASE;
+       end if;
+    end process;
+    
     -- gestione uscite con DONE
-    process(i_clk)
+    process(i_clk, temp_done)
     begin 
         if temp_done = '0' then
             o_z0 <= "00000000";
@@ -164,44 +170,65 @@ BEGIN
         
     PROCESS (i_clk, i_rst) --FSM
     BEGIN
+    o_mem_en <= '0';
+    addr_en <= '0';
+    chan_en <= '0';
+    internal_rst <= '0';
+    temp_done <= '0';
+    receive <= '0';
+    
         CASE state IS
             when S0 =>
-                internal_rst <= '0';
                 temp_done <= '0';
+                internal_rst <= '0';
+            when S1 => -- legge primo bit canale
+                chan_en <= '1';
+            when S2 => -- legge secondo bit di indirizzo
+                chan_en <= '1';
+            when S3 => -- legge indirizzo bit a bit
+                addr_en <= '1';
+            when S4 => -- trasmette indirizzo
+                o_mem_en <= '1';
+            when S5 => -- ricevi data da memoria
+                receive <= '1';
+            when S6 => -- espone risultati, resetta addr register
+                temp_done <= '1';
+                internal_rst <= '1';
+        END CASE;
+    END PROCESS;
+
+    PROCESS (i_clk, i_rst, i_start) --FSM
+    BEGIN
+        next_state <= state; --c
+        CASE state IS
+            when S0 =>
+                next_state <= S0;
                 if i_start = '1' then 
                     next_state <= S1;
                 end if;
             when S1 => -- legge primo bit canale
-                addr_en <= '0';
-                chan_en <= '1';
                 next_state <= S2;
             when S2 => -- legge secondo bit di indirizzo
-                addr_en <= '0';
-                chan_en <= '1';
-                if i_start = '1' then 
-                    next_state <= S3;
-                else next_state <= S4;
-                end if;
+                -- next_state <= S3;
+                if i_start = '1' then next_state <= S3; 
+                else next_state <= S4; end if;
             when S3 => -- legge indirizzo bit a bit
-                addr_en <= '1';
-                chan_en <= '0';
-                if i_start = '1' then next_state <= S3;
-                else next_state <= S4;
-                end if;
+                next_state <= S3;
+                if i_start = '0' then next_state <= S4; end if;
+                -- if i_start = '1' then 
+                 --   next_state <= S3;
+                --else next_state <= S4;
+                -- end if;
             when S4 => -- trasmette indirizzo
-                o_mem_en <= '1';
-                addr_en <= '0';
-                chan_en <= '0';
                 next_state <= S5;
             when S5 => -- ricevi data da memoria
-                receive <= '1';
                 next_state <= S6;
             when S6 => -- espone risultati, resetta addr register
-                temp_done <= '1';
-                internal_rst <= '1';
                 next_state <= S0;         
         END CASE;
     END PROCESS;
     
     o_done <= temp_done;
 END Behavioral;
+
+
