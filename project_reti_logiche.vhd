b052abb76aefecf37c7e031c8e1c27c9a8284a6b
PGawain <piermarco.gerli@gmail.com>
2023-03-27 10:36:14 +0200

    Aggiunti commenti

--- project_reti_logiche.vhd
+++ project_reti_logiche.vhd
@@ -1,30 +1,15 @@
 ----------------------------------------------------------------------------------
 -- Company: Politecnico di Milano
--- Engineer: Gabriele Gessaghi, Piermarco Gerli
+-- Engineer: Piermarco Gerli   (cod_persona: 10729408, matricola: 955980)
+--           Gabriele Gessaghi (cod_persona: 10660853, matricola: 939491)
 -- 
--- Create Date: 01.03.2023 15:31:55
--- Design Name: 
--- Module Name: project_reti_logiche - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
+-- Module Name: project_reti_logiche
+-- Project Name: Prova Finale di Reti Logiche (professore F.Salice)
 -- 
 ----------------------------------------------------------------------------------
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
--- use IEEE.STD_LOGIC_UNSIGNED.all;
-
 
 ENTITY project_reti_logiche IS
     PORT (
@@ -46,26 +31,30 @@ END project_reti_logiche;
 
 ARCHITECTURE Behavioral OF project_reti_logiche IS
 
+    -- Segnali di controllo interni al componente
     SIGNAL sum_address : STD_LOGIC_VECTOR (15 DOWNTO 0);
     SIGNAL sum_channel : STD_LOGIC_VECTOR (1 DOWNTO 0);
     SIGNAL channel_selector : STD_LOGIC_VECTOR (1 DOWNTO 0);
     SIGNAL addr_en : STD_LOGIC;
     SIGNAL chan_en : STD_LOGIC;
     SIGNAL receive : STD_LOGIC;
-    -- ch_enable pack
-    SIGNAL ch0_en : STD_LOGIC;
-    SIGNAL ch1_en : STD_LOGIC;
-    SIGNAL ch2_en : STD_LOGIC;
-    SIGNAL ch3_en : STD_LOGIC;
     SIGNAL internal_rst : STD_LOGIC;
     SIGNAL temp_done : STD_LOGIC;
     signal ingresso : std_logic;
-    
+
+    -- Registri delle uscite
     SIGNAL reg_z0 : STD_LOGIC_VECTOR (7 DOWNTO 0);
     SIGNAL reg_z1 : STD_LOGIC_VECTOR (7 DOWNTO 0);
     SIGNAL reg_z2 : STD_LOGIC_VECTOR (7 DOWNTO 0);
     SIGNAL reg_z3 : STD_LOGIC_VECTOR (7 DOWNTO 0);
+    
+    -- Segnali di enable per i registri
+    SIGNAL ch0_en : STD_LOGIC;
+    SIGNAL ch1_en : STD_LOGIC;
+    SIGNAL ch2_en : STD_LOGIC;
+    SIGNAL ch3_en : STD_LOGIC;
 
+    -- Stati e segnali di gestione della FSM
     TYPE state_type IS (S0, S1, S2, S3, S4, S5, S6);
     SIGNAL state : state_type;
     SIGNAL next_state : state_type;
@@ -77,55 +66,33 @@ BEGIN
         if rising_edge(i_clk) then ingresso <= i_w; end if;
     end process;
     
-    PROCESS (i_clk, i_rst, internal_rst) -- processo calcolo address
+    ----------------------------------
+    ----  MODULO DI ACQUISIZIONE  ----
+    ----------------------------------
+    PROCESS (i_clk, i_rst, internal_rst) -- Processo di acquisizione dell'indirizzo
     BEGIN
         IF (i_rst = '1') or internal_rst = '1' THEN
             sum_address <= (others => '0');
         ELSIF rising_edge(i_clk) and addr_en = '1' THEN
-            -- sum_address <= std_logic_vector(unsigned(sum_address) sll 1); -- shift logico sx del canale
-            -- sum_address(0) <= '1';
             sum_address <= sum_address(14 downto 0) & ingresso;
         END IF;
     END PROCESS;
     
     o_mem_addr <= sum_address;
 
-    PROCESS (i_clk, i_rst, internal_rst) -- processo selezione canale di uscita
+    PROCESS (i_clk, i_rst, internal_rst) -- Processso di acquisizione del canale sul quale indirizzare le uscite
     BEGIN
         IF (i_rst = '1') or internal_rst = '1' THEN
             channel_selector <= (others => '0');
         ELSIF rising_edge(i_clk) and chan_en = '1' THEN
-            -- channel_selector <= std_logic_vector(unsigned(channel_selector) sll 1); -- shift logico sx del canale
-            -- channel_selector(0) <= '1';
             channel_selector <= channel_selector(0) & ingresso;
         END IF;
     END PROCESS;
-
-    -- indirizzamento data su uscita indicata in channel
-    process (i_clk, i_rst)
-    begin
-        if i_rst = '1' then
-            reg_z0 <= "00000000";
-            reg_z1 <= "00000000";
-            reg_z2 <= "00000000";
-            reg_z3 <= "00000000";
-        elsif rising_edge(i_clk) then
-            if ch0_en = '1' then 
-                    reg_z0 <= i_mem_data;
-                end if; 
-                if ch1_en = '1' then 
-                    reg_z1 <= i_mem_data;
-                end if;
-                if ch2_en = '1' then 
-                    reg_z2 <= i_mem_data;
-                end if;
-                if ch3_en = '1' then 
-                    reg_z3 <= i_mem_data;
-                end if;
-        end if;
-    end process;
     
-    process(i_clk, receive) -- selezione canale uscita ed enabler mux
+    ------------------------------------
+    ----  MODULO DI INDIRIZZAMENTO  ----
+    ------------------------------------
+    process(i_clk, receive) -- Processo per attivare il registro corrispondente al valore indicato da "channel_selector"
     begin
         ch0_en <= '0'; 
         ch1_en <= '0'; 
@@ -141,9 +108,31 @@ BEGIN
             END CASE;
        end if;
     end process;
-    
-    -- gestione uscite con DONE
-    process(i_clk, temp_done)
+
+    process (i_clk, i_rst) -- Processo per caricare i dati sul registro dell'uscita indicata
+    begin
+        if i_rst = '1' then
+            reg_z0 <= "00000000";
+            reg_z1 <= "00000000";
+            reg_z2 <= "00000000";
+            reg_z3 <= "00000000";
+        elsif rising_edge(i_clk) then
+            if ch0_en = '1' then 
+                reg_z0 <= i_mem_data;
+            end if; 
+            if ch1_en = '1' then 
+                reg_z1 <= i_mem_data;
+            end if;
+            if ch2_en = '1' then 
+                reg_z2 <= i_mem_data;
+            end if;
+            if ch3_en = '1' then 
+                reg_z3 <= i_mem_data;
+            end if;
+        end if;
+    end process;
+
+    process(i_clk, temp_done) -- Processo per "esporre" i dati caricati dalla memoria
     begin 
         if temp_done = '1' then
             o_z0 <= reg_z0;
@@ -157,20 +146,20 @@ BEGIN
             o_z3 <= "00000000"; 
         end if;
     end process;
-    
-    PROCESS (i_clk, i_rst) -- aggiornatore FSM
+
+
+    -----------------------------------
+    ----  Macchina a Stati Finiti  ----
+    -----------------------------------
+    PROCESS (i_clk, i_rst) -- Processo che aggiorna lo stato della Macchina a Stati Finiti (FSM)
     BEGIN 
         if i_rst = '1' then state <= S0;
         elsif rising_edge(i_clk) then state <= next_state;
         end if;
     
     END PROCESS;
-   -- process( i_clk)
-   -- begin
-        -- if rising_edge(i_clk) then o_done <= temp_done; end if;
-   -- end process;
         
-    PROCESS (i_clk, i_rst) --FSM
+    PROCESS (i_clk, i_rst) -- FSM (parte 1/2), gestione dei segnali di controllo interni del componente
     BEGIN
     o_mem_en <= '0';
     addr_en <= '0';
@@ -178,55 +167,48 @@ BEGIN
     internal_rst <= '0';
     temp_done <= '0';
     receive <= '0';
-    
         CASE state IS
             when S0 =>
                 temp_done <= '0';
                 internal_rst <= '0';
-            when S1 => -- legge primo bit canale
+            when S1 => -- Acquisizione primo bit del canale di uscita
                 chan_en <= '1';
-            when S2 => -- legge secondo bit di indirizzo
+            when S2 => -- Acquisizione secondo bit del canale di uscita
                 chan_en <= '1';
-            when S3 => -- legge indirizzo bit a bit
+            when S3 => -- Acquisizione indirizzo
                 addr_en <= '1';
-            when S4 => -- trasmette indirizzo
+            when S4 => -- Trasmissione indirizzo alla memoria
                 o_mem_en <= '1';
-            when S5 => -- ricevi data da memoria
+            when S5 => -- Ricezione dato da memoria
                 receive <= '1';
-                --temp_done <= '1';
-            when S6 => -- espone risultati, resetta addr register
+            when S6 => -- Esposizione dato sulle uscite, preparazione per nuova lettura
                 temp_done <= '1';
                 internal_rst <= '1';
         END CASE;
     END PROCESS;
 
-    PROCESS (i_clk, i_rst, i_start) --FSM
+    PROCESS (i_clk, i_rst, i_start) --FSM (parte 2/2), gestione dello stato prossimo
     BEGIN
-        next_state <= state; --c
+        next_state <= state;
         CASE state IS
             when S0 =>
                 next_state <= S0;
                 if i_start = '1' then 
                     next_state <= S1;
                 end if;
-            when S1 => -- legge primo bit canale
+            when S1 => 
                 next_state <= S2;
-            when S2 => -- legge secondo bit di indirizzo
-                -- next_state <= S3;
+            when S2 =>
                 if i_start = '1' then next_state <= S3; 
                 else next_state <= S4; end if;
-            when S3 => -- legge indirizzo bit a bit
+            when S3 => 
                 next_state <= S3;
                 if i_start = '0' then next_state <= S4; end if;
-                -- if i_start = '1' then 
-                 --   next_state <= S3;
-                --else next_state <= S4;
-                -- end if;
-            when S4 => -- trasmette indirizzo
+            when S4 => 
                 next_state <= S5;
-            when S5 => -- ricevi data da memoria
+            when S5 => 
                 next_state <= S6;
-            when S6 => -- espone risultati, resetta addr register
+            when S6 =>
                 next_state <= S0;         
         END CASE;
     END PROCESS;
